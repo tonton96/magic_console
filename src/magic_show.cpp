ee05970328e25f06599501324b831758068978c6
vinhlc <lcv0611@gmail.com>
2022-10-20 01:34:24 +0700

    done

--- src/magic_show.cpp
+++ src/magic_show.cpp
@@ -1,4 +1,86 @@
+#include <random>
 #include "magic_show.h"
+
+void move_object(double **points, int num_points, double *center, const double *position) {
+    for (int j = 0; j < DIM; j++) {
+        double delta = position[j] - center[j];
+        for (int i = 0; i < num_points; i++) {
+            points[j][i] += delta;
+        }
+        center[j] = position[j];
+    }
+}
+
+void add_heart(double size, double **points, char *color_points, int *num_points, double *center) {
+    const int num_bgs = 2;
+    const std::string bgs[num_bgs] = {"|Happy Viet Nam women's day 20/10|", "|I <3 U|"};
+    const char line = (char)219;
+
+    int len_bgs[num_bgs];
+    for (int i = 0; i < num_bgs; i++) {
+        len_bgs[i] = (int) bgs[i].size();
+    }
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distr(0, num_bgs - 1);
+    int bid = distr(gen);
+
+    const double step_x = 0.5, step_y = 0.5;
+    int pid = 0, bi = 0;
+    center[0] = center[1] = center[2] = 0;
+
+    double x = -2 * size;
+    while (x <= 2 * size) {
+        double y = std::sqrt(size * size - (std::abs(x) - size) * (std::abs(x) - size));
+        points[0][pid] = x;
+        points[1][pid] = y;
+        points[2][pid] = 0;
+        color_points[pid] = line;
+        pid++;
+
+        center[0] += x;
+        center[1] += y;
+
+        if (bid < num_bgs) {
+            double y0 = size * std::acos(1 - std::abs(x / size)) - size * M_PI;
+            while (y > y0) {
+                points[0][pid] = x;
+                points[1][pid] = y;
+                points[2][pid] = 0;
+                color_points[pid] = bgs[bid][bi++];
+                if (bi == len_bgs[bid]) {
+                    bi = 0;
+                }
+                pid++;
+
+                center[0] += x;
+                center[1] += y;
+                y -= step_y;
+            }
+        }
+
+        x += step_x;
+    }
+
+    x = -2 * size;
+    while (x <= 2 * size) {
+        double y = size * std::acos(1 - std::abs(x / size)) - size * M_PI;
+        points[0][pid] = x;
+        points[1][pid] = y;
+        points[2][pid] = 0;
+        color_points[pid] = line;
+        pid++;
+
+        center[0] += x;
+        center[1] += y;
+        x += step_x;
+    }
+
+    *num_points = pid;
+    center[0] /= pid;
+    center[1] /= pid;
+}
 
 void render(double ***all_points, char **color_points, const int *num_points, int num_objects,
             const int *active_objects, std::string &screen) {
@@ -10,7 +92,7 @@ void render(double ***all_points, char **color_points, const int *num_points, in
         }
     }
     for (int id = 0; id < num_objects; id++) {
-        if(active_objects[id]) {
+        if (active_objects[id]) {
             for (int n = 0; n < num_points[id]; n++) {
                 int w = (int) (all_points[id][0][n] / WIDTH_RATE);
                 int h = (int) (all_points[id][1][n] / HEIGHT_RATE);
@@ -45,27 +127,80 @@ void show_magic() {
         center_objects[i] = (double *) malloc(DIM * sizeof(double));
     }
 
-    uint64_t sum_time = 300 * 1000;    //microseconds
+    auto trans_x = (double *) malloc(MAX_NUM_OBJECTS * sizeof(double));
+    auto trans_y = (double *) malloc(MAX_NUM_OBJECTS * sizeof(double));
+    auto rotate_x = (double *) malloc(MAX_NUM_OBJECTS * sizeof(double));
+    auto rotate_y = (double *) malloc(MAX_NUM_OBJECTS * sizeof(double));
+    auto rotate_z = (double *) malloc(MAX_NUM_OBJECTS * sizeof(double));
+    auto axis = malloc_double_matrix(MAX_NUM_OBJECTS, DIM);
+
+    uint64_t sum_time = 300 * 1000;
     uint64_t crt_time = 0;
 
-    int crt_id = 0;
-    add_heart(8, points[crt_id], color_points[crt_id], &num_points[crt_id], center_objects[crt_id]);
+    int time_span = 0, time_span0 = 0;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> time_rd(TIME_SPAN0, TIME_SPAN1);
+    std::uniform_int_distribution<int> heart_rd(HEART_SIZE_0, HEART_SIZE_1);
+    std::uniform_real_distribution<double> trans_x_rd(TRANS0, TRANS1);
+    std::uniform_real_distribution<double> trans_y_rd(TRANS0, TRANS1 * 2);
+    std::uniform_real_distribution<double> rotate_x_rd(0, ROTATE0);
+    std::uniform_real_distribution<double> rotate_y_rd(0, ROTATE1);
+    std::uniform_real_distribution<double> rotate_z_rd(0, ROTATE0);
+    std::uniform_real_distribution<double> pos_x_rd(0, WIDTH * WIDTH_RATE);
+    std::uniform_real_distribution<double> pos_z_rd(0, WIDTH * WIDTH_RATE);
 
     while (crt_time < sum_time) {
-        auto start = std::chrono::high_resolution_clock::now();
+        if (time_span >= time_span0) {
+            time_span = 0;
+            time_span0 = time_rd(gen);
+            for (int i = 0; i < MAX_NUM_OBJECTS; i++) {
+                if (!active_objects[i]) {
+                    active_objects[i] = true;
 
-        rotate3d_x(points[crt_id], num_points[crt_id], 0.2, center_objects[crt_id]);
-        rotate3d_y(points[crt_id], num_points[crt_id], 0.2, center_objects[crt_id]);
+                    add_heart(heart_rd(gen), points[i], color_points[i], &num_points[i], center_objects[i]);
+                    rotate3d_z(points[i], num_points[i], M_PI, center_objects[i]);
+
+                    double new_pos[3] = {pos_x_rd(gen), Y0, pos_z_rd(gen)};
+                    move_object(points[i], num_points[i], center_objects[i], new_pos);
+
+                    trans_x[i] = 1;
+                    trans_y[i] = trans_y_rd(gen);
+                    rotate_x[i] = rotate_x_rd(gen);
+                    rotate_y[i] = rotate_y_rd(gen);
+                    rotate_z[i] = rotate_z_rd(gen);
+                    axis[i][0] = center_objects[i][0] + pos_x_rd(gen) / 10;
+                    axis[i][1] = Y0;
+                    axis[i][2] = center_objects[i][2] + pos_z_rd(gen) / 10;
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < MAX_NUM_OBJECTS; i++) {
+            if (active_objects[i]) {
+                rotate3d_x(points[i], num_points[i], rotate_x[i], center_objects[i]);
+                rotate3d_y(points[i], num_points[i], rotate_y[i], axis[i]);
+                rotate3d_z(points[i], num_points[i], rotate_z[i], center_objects[i]);
+                trans3d(points[i], num_points[i], trans_x[i], trans_y[i], 0);
+                center_objects[i][0] += trans_x[i];
+                center_objects[i][1] += trans_y[i];
+            }
+        }
 
         render(points, color_points, num_points, MAX_NUM_OBJECTS, active_objects, screen);
 
-        std::system("cls");
-        std::cout << screen;
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::cout << "\r" << screen;
+        std::this_thread::sleep_for(std::chrono::milliseconds(TIME_LOOP));
 
-        auto stop = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
-        crt_time += duration.count();
+        for (int i = 0; i < MAX_NUM_OBJECTS; i++) {
+            if (center_objects[i][1] > 1.2 * HEIGHT_RATE * HEIGHT) {
+                active_objects[i] = false;
+            }
+        }
+
+        crt_time += TIME_LOOP;
+        time_span += TIME_LOOP;
     }
 
     for (int i = 0; i < MAX_NUM_OBJECTS; i++) {
@@ -78,4 +213,11 @@ void show_magic() {
     free(num_points);
     free(color_points);
     free(center_objects);
+
+    free(trans_x);
+    free(trans_y);
+    free(rotate_x);
+    free(rotate_y);
+    free(rotate_z);
+    free_matrix(axis, MAX_NUM_OBJECTS);
 }
--- src/magic_show.h
+++ src/magic_show.h
@@ -1,16 +1,30 @@
 #ifndef MAGIC_MAGIC_SHOW_H
 #define MAGIC_MAGIC_SHOW_H
 
-#include "object_creator.h"
+#include "math_handler.h"
 
-#define MAX_NUM_OBJECTS 50
-#define MAX_POINTS      2048
+#define MAX_NUM_OBJECTS 15
+#define MAX_POINTS      10240
 #define DIM             3
-#define WIDTH           100
-#define HEIGHT          50
+#define WIDTH           250
+#define HEIGHT          60
 #define MAX_Z           1e6
 #define WIDTH_RATE      1
-#define HEIGHT_RATE     1.5
+#define HEIGHT_RATE     2
+#define TIME_SPAN0      1000
+#define TIME_SPAN1      2000
+#define TIME_LOOP       100
+#define TRANS0          0.5
+#define TRANS1          1.0
+#define ROTATE0         0.02
+#define ROTATE1         0.1
+#define Y0              -10
+#define HEART_SIZE_0    3
+#define HEART_SIZE_1    8
+
+void add_heart(double size, double **points, char *color_points, int *num_points, double *center);
+
+void move_object(double **points, int num_points, double *center, const double *position);
 
 void render(double ***all_points, char **color_points, const int *num_points, int num_objects, std::string &screen);
 
